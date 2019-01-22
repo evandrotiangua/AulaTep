4bc3996278c2b7162c6cb23a9d1be08e840619df
EvandroTiangua <evandrotiangua@gmail.com>
2019-01-22 20:01:58 -0300

    Mensagem de Boa Noite

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,9 @@
 package Aplicacao;
 
 public class Programa {
-
 	public static void main(String[] args) {
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde");
+		System.out.println("Mensagem de boa noite");
 	}
-
 }
